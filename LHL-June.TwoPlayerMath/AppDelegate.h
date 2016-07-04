6c82038a29352d63a57824531ae8e618c231e1c2
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-07-04 16:38:47 -0700

    June. My initial commit

---
+++ LHL-June.TwoPlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHL-June.TwoPlayerMath
+//
+//  Created by Asuka Nakagawa on 2016-07-04.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHL-June.TwoPlayerMath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHL-June.TwoPlayerMath
+//
+//  Created by Asuka Nakagawa on 2016-07-04.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
